69348e3cf78aa7bec52032635af97a9389706703
a2thedev <anthonygreen048@gmail.com>
2023-12-05 20:26:31 -0500

    added movieListComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import MovieList from "./components/MovieList/MovieList";
+<components />;
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList />
     </div>
   );
 }

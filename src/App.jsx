776473d8a1e3d193c2dff9f58bed67cede294a7c
a2thedev <anthonygreen048@gmail.com>
2023-12-08 11:10:17 -0500

    Lifting state

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import Header from "./components/Header/Header";
 import MovieList from "./components/MovieList/MovieList";
 import MovieInfo from "./components/MovieInfo/Movieinfo";
 import NewMovieForm from "./components/NewMovieForm/NewMovieForm";
+import React, { useState } from "react";
+
 import axios from "axios";
 function App() {
+  const [movies, setMovies] = useState();
+
   const selectedMovie = {
     title: "Forrest Gump",
     runningTime: 142,
@@ -18,7 +22,7 @@ function App() {
     <div className="App">
       <Header />
       <div className="flex-container">
-        <MovieList />
+        <MovieList movies={movies} />
         <MovieInfo movieObj={selectedMovie} />
         <NewMovieForm />
       </div>
